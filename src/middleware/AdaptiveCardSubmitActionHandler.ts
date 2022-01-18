4f73a7f6d778e4170bb3b68dfe15bd4c5fc1a57c
lenhat341999 <lenhat3419993@gmail.com>
2022-01-18 21:37:52 +0700

    3

--- src/middleware/AdaptiveCardSubmitActionHandler.ts
+++ src/middleware/AdaptiveCardSubmitActionHandler.ts
@@ -1,5 +1,5 @@
 import * as builder from "botbuilder";
-// Sửa lần 2
+// Sửa lần 3
 // Set the text field on message events to "adaptive card", if request is from an adaptive card
 export class AdaptiveCardSubmitActionHandler implements builder.IMiddlewareMap {
 
